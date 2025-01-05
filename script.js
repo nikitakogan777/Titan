1d7769c5ece96113cbd6081b7f15f54587a2eb4a
nikitakogan <dashgerot@gmail.com>
2025-01-05 15:28:36 +0700

    <club titan>

---
+++ script.js
@@ -0,0 +1,55 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const phoneLink = document.querySelector('.phone-link');
+
+  if (phoneLink) {
+    const isMobile = /Mobi|Android|iPhone|iPad/i.test(navigator.userAgent);
+
+    if (!isMobile) {
+      phoneLink.removeAttribute('href');
+      phoneLink.style.cursor = 'default';
+    }
+  }
+
+  function addClassOnIntersect(className, targetClass, threshold = 0.5) {
+    function onEntry(entry) {
+      entry.forEach(change => {
+        if (change.isIntersecting) {
+          change.target.classList.add(className);
+        }
+      });
+    }
+
+    const options = {
+      threshold: [threshold]
+    };
+
+    const observer = new IntersectionObserver(onEntry, options);
+    const elements = document.querySelectorAll(targetClass);
+
+    for (let elm of elements) {
+      observer.observe(elm);
+    }
+  }
+
+  addClassOnIntersect('images__background', '.hero__body');
+  addClassOnIntersect('hero_title', '.description__container');
+  addClassOnIntersect('description__header', '.achievements__items');
+  addClassOnIntersect('items', '.social__body');
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const openButton = document.getElementById('openMenuButton');
+  const dialog = document.getElementById('mobileOverlay');
+
+  openButton.addEventListener('click', () => {
+    dialog.showModal();
+    openButton.classList.add('active');
+  });
+
+  dialog.addEventListener('click', (event) => {
+    if (event.target === dialog) {
+      dialog.close();
+      openButton.classList.remove('active');
+    }
+  });
+});
